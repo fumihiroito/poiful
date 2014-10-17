6b323b5c0b1c367b31c2e1abf721454b033a04c7
Fumihiro Ito <fumihiro.ito@mixi.co.jp>
2014-10-17 15:32:43 +0900

    support official rblineprof gem

--- lib/poiful/profiler.rb
+++ lib/poiful/profiler.rb
@@ -9,22 +9,35 @@ module Poiful
       profile = lineprof(/./, &block)
       print Renderer.text(profile)
     end
+  end
+end
 
-    def begin
-      RbLineProf.begin(/./)
-    end
+class_exist = begin
+                klass = Module.const_get("RbLineProf")
+                klass.is_a?(Class)
+              rescue
+                false
+              end
 
-    def end
-      @@profile_result = RbLineProf.end
-      @@profile_result
-    end
+if class_exist
+  module Poiful
+    class << self
+      def begin
+        RbLineProf.begin(/./)
+      end
 
-    def show
-      print Renderer.text(@@profile_result)
-    end
+      def end
+        @@profile_result = RbLineProf.end
+        @@profile_result
+      end
 
-    def render_html
-      Renderer.html(@@profile_result, "prof")
+      def show
+        print Renderer.text(@@profile_result)
+      end
+
+      def render_html
+        Renderer.html(@@profile_result, "prof")
+      end
     end
   end
 end
